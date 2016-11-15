ecb6efa962ceccf2a068bc0c17cd2cb31ea7101f
Студент <@1eb50117b7c949b>
2016-11-15 17:41:17 +0200

    fix

--- src/java1/JAVA1.java
+++ src/java1/JAVA1.java
@@ -15,6 +15,8 @@ public class JAVA1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Батренюк" + " Александра" + " Владимировна");// ФИО
+        System.out.println("12 марта 1998");
         // TODO code application logic here
     }
     
